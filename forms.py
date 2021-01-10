05b642217d1b40e2af3e3d47f82979d00aacc979
Luke <deandrevidal@aol.com>
2021-01-10 07:04:26 -0800

    snapshot v1

--- forms.py
+++
@@ -1,5 +0,0 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
-
-class ContactForm(Form):
-    name = StringField('Full Name', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
